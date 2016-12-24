f0ac4e13da8a914a3b400a1b20a3f808f249e203
Eduardo Loeffel Noce Gobi <eduardogobi@outlook.com>
2016-12-24 21:03:21 -0200

    Initial commit

---
+++ src/io/github/egobi/zhemchuzhnikovita/Core.java
@@ -0,0 +1,13 @@
+package io.github.egobi.zhemchuzhnikovita;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Core extends JavaPlugin {
+	// Para quando o plugin estiver ativado
+	@Override
+	public void onEnable() {
+	}
+	// Para quando o plugin estiver desativado
+	@Override
+	public void onDisable() {
+	}
+}
